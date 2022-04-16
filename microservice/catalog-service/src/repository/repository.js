f3fa754898a86364b60aa8cd22afe7273cd9250a
W. Guilherme <wguilhermecriativo@gmail.com>
2022-04-16 11:27:09 -0300

    feat: add catalog microservice repository and api

--- microservice/catalog-service/src/repository/repository.js
+++ microservice/catalog-service/src/repository/repository.js
@@ -1,24 +1,76 @@
-const mongodb = require('../config/mongodb')
-const ObjectId = require('mongodb').ObjectId
+//repository.js
+const database = require("../config/database");
+const { ObjectId } = require("mongodb");
 
-function getAllCities(callback) {
-  mongodb.connect((err, db) => {
-    db.collection("cinemaCatalog").find({}, { cidade: 1, uf: 1, pais: 1 }).toArray(callback)
-  })
+async function getAllCities() {
+  const db = await database.connect();
+  return db.collection("cinemaCatalog").find({}).projection({ cidade: 1, uf: 1, pais: 1 }).toArray();
 }
 
-function getCinemaByCityId(cityId, callback) {
-  const objectCityId = ObjectId(cityId)
-  mongodb.connect((err, db) => {
-    db.collection("cinemaCatalog").find({ _id: objectCityId }, { cinemas: 1 })
-      .toArray((err, cities) => {
-        callback(err, cities[0].cinemas)
-      })
-  })
+async function getCinemasByCityId(cityId) {
+  const objCityId = new ObjectId(cityId);
+  const db = await database.connect();
+  return db.collection('cinemaCatalog')
+    .findOne({ _id: objCityId }, { projection: { cinemas: 1 } });
 }
 
-function disconnect() {
-  return mongodb.disconnect()
+async function disconnect() {
+  return database.disconnect();
 }
 
-module.exports = { getAllCities, getCinemaByCityId, disconnect }
+async function getMoviesByCinemaId(cinemaId) {
+  const objCinemaId = new ObjectId(cinemaId);
+  const db = await database.connect();
+  return db.collection("cinemaCatalog").aggregate([
+    { $match: { "cinemas._id": objCinemaId } },
+    { $unwind: "$cinemas" },
+    { $unwind: "$cinemas.salas" },
+    { $unwind: "$cinemas.salas.sessoes" },
+    { $group: { _id: { filme: "$cinemas.salas.sessoes.filme", idFilme: "$cinemas.salas.sessoes.idFilme" } } }
+  ]).toArray();
+}
+
+async function getMoviesByCityId(cityId) {
+  const objCityId = new ObjectId(cityId);
+  const db = await database.connect();
+  const sessions = await db.collection("cinemaCatalog").aggregate([
+    { $match: { "_id": objCityId } },
+    { $unwind: "$cinemas" },
+    { $unwind: "$cinemas.salas" },
+    { $unwind: "$cinemas.salas.sessoes" },
+    { $group: { _id: { filme: "$cinemas.salas.sessoes.filme", idFilme: "$cinemas.salas.sessoes.idFilme" } } }
+  ]).toArray();
+  return sessions.map(item => item._id);
+}
+
+async function getMovieSessionsByCityId(movieId, cityId) {
+  const objMovieId = new ObjectId(movieId);
+  const objCityId = new ObjectId(cityId);
+  const db = await database.connect();
+  const sessions = await db.collection("cinemaCatalog").aggregate([
+    { $match: { "_id": objCityId } },
+    { $unwind: "$cinemas" },
+    { $unwind: "$cinemas.salas" },
+    { $unwind: "$cinemas.salas.sessoes" },
+    { $match: { "cinemas.salas.sessoes.idFilme": objMovieId } },
+    { $group: { _id: { filme: "$cinemas.salas.sessoes.filme", idFilme: "$cinemas.salas.sessoes.idFilme", idCinema: "$cinemas._id", sala: "$cinemas.salas.nome", sessao: "$cinemas.salas.sessoes" } } }
+  ]).toArray();
+  return sessions.map(item => item._id);
+}
+
+async function getMovieSessionsByCinemaId(movieId, cinemaId) {
+  const objCinemaId = new ObjectId(cinemaId);
+  const objMovieId = new ObjectId(movieId);
+  const db = await database.connect();
+  const sessions = await db.collection("cinemaCatalog").aggregate([
+    { $match: { "cinemas._id": objCinemaId } },
+    { $unwind: "$cinemas" },
+    { $unwind: "$cinemas.salas" },
+    { $unwind: "$cinemas.salas.sessoes" },
+    { $match: { "cinemas.salas.sessoes.idFilme": objMovieId } },
+    { $group: { _id: { filme: "$cinemas.salas.sessoes.filme", idFilme: "$cinemas.salas.sessoes.idFilme", sala: "$cinemas.salas.nome", sessao: "$cinemas.salas.sessoes" } } }
+  ]).toArray();
+  return sessions.map(item => item._id);
+}
+
+module.exports = { getAllCities, getCinemasByCityId, disconnect, getMoviesByCinemaId, getMoviesByCityId, getMovieSessionsByCityId, getMovieSessionsByCinemaId }
--- microservice/catalog-service/src/repository/repository.test.js
+++ microservice/catalog-service/src/repository/repository.test.js
@@ -1,18 +1,57 @@
-const test = require('tape')
-const repository = require('./repository')
+require('dotenv-safe').config();
+const repository = require('./repository');
+let testCityId = null;
+let testCinemaId = null;
+let testMovieId = null;
 
-function runTests() {
-  let cityId = null
-  let cinemaId = null
-  let movieId = null
+beforeAll(async () => {
+  const cities = await repository.getAllCities();
+  testCityId = cities[1]._id;//Porto Alegre
+  testCinemaId = cities[1].cinemas[0]._id;
+  testMovieId = cities[1].cinemas[0].salas[0].sessoes[0].idFilme;
+})
 
-  test('Repository getAllCities', (t) => {
-    repository.getAllCities((err, cities) => {
-      if (cities && cities.length > 0) cities[1]._id; //porto alegre
-      t.assert(!err && cities && cities.length > 0, "All cities returned")
-      t.end()
-    })
-  })
+afterAll(async () => {
+  await repository.disconnect();
+})
 
+test('Repository getAllCities', async () => {
+  const cities = await repository.getAllCities();
+  expect(Array.isArray(cities)).toBeTruthy();
+  expect(cities.length).toBeGreaterThan(0);
+})
+
+test('Repository getCinemasByCityId', async () => {
+  const cinemas = await repository.getCinemasByCityId(testCityId);
+  expect(Array.isArray(cinemas)).toBeTruthy();
+  expect(cinemas.length).toBeGreaterThan(0);
+})
+
+test('Repository Disconnect', async () => {
+  const isDisconnected = await repository.disconnect();
+  expect(isDisconnected).toBeTruthy();
+})
+
+test('Repository getMoviesByCinemaId', async () => {
+  const result = await repository.getMoviesByCinemaId(testCinemaId);
+  expect(Array.isArray(result)).toBeTruthy();
+  expect(result.length).toBeGreaterThan(0);
+})
+
+test('Repository getMoviesByCityId', async () => {
+  const result = await repository.getMoviesByCityId(testCityId);
+  expect(Array.isArray(result)).toBeTruthy();
+  expect(result.length).toBeGreaterThan(0);
+})
+
+test('Repository getMovieSessionsByCityId', async () => {
+  const result = await repository.getMovieSessionsByCityId(testMovieId, testCityId);
+  expect(Array.isArray(result)).toBeTruthy();
+  expect(result.length).toBeGreaterThan(0);
+})
+
+test('Repository getMovieSessionsByCinemaId', async () => {
+  const result = await repository.getMovieSessionsByCinemaId(testMovieId, testCinemaId);
+  expect(Array.isArray(result)).toBeTruthy();
+  expect(result.length).toBeGreaterThan(0);
 }
-module.exports = { runTests }
