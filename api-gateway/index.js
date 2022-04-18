9f8f47ccff1539cf891bbed8e59355193827ffc7
W. Guilherme <wguilhermecriativo@gmail.com>
2022-04-18 17:23:01 -0300

    feat: add login route + generate auth jwt token

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,3 +1,5 @@
+require('dotenv-safe').config();
+const jwt = require('jsonwebtoken')
 const express = require('express')
 const httpProxy = require('express-http-proxy')
 var cookieParser = require('cookie-parser');
@@ -6,9 +8,30 @@ const helmet = require('helmet');
 
 var http = require('http');
 const app = express()
+app.use(logger('dev'));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 const catalogServiceProxy = httpProxy('http://localhost:3001');
 const moviesServiceProxy = httpProxy('http://localhost:3002');
+
+
+
+
+app.post('/login', (req, res, next) => {
+
+  if (req.body.user === 'app@app.com' && req.body.password === '12345678') {
+    const id = 1
+    //expires in 5min
+    const token = jwt.sign({ id }, process.env.SECRET, { expiresIn: 300 })
+    res.status(200).send({ auth: true, token: token })
+  } else {
+
+    res.status(500).send('Invalid user credentials')
+  }
+})
 
 
 app.get('/cities', (req, res, next) => {
@@ -17,11 +40,7 @@ app.get('/cities', (req, res, next) => {
 app.get('/movies', (req, res, next) => {
   moviesServiceProxy(req, res, next);
 })
-app.use(logger('dev'));
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 
 var server = http.createServer(app);
 server.listen(3000, () => console.log('API Gateway is running at port 3000'));
